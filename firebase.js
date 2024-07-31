d954cf812d35cf4cbd36432d32324dd4823a4f06
Aseef Durrani <aseef.durrani11@gmail.com>
2024-07-30 23:06:16 -0400

    env update

--- firebase.js
+++ firebase.js
@@ -8,20 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  //   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  //   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  //   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  //   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  //   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-  apiKey: "AIzaSyByq2X30lN4tlVNAw9E8P5yEaQmwcTc3FA",
-  authDomain: "headstarter-pantryapp.firebaseapp.com",
-  projectId: "headstarter-pantryapp",
-  storageBucket: "headstarter-pantryapp.appspot.com",
-  messagingSenderId: "781128256241",
-  appId: "1:781128256241:web:8eff3214e08be5622f7750",
-  measurementId: "G-KZK846EVXF",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
